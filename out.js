a818f676bfe7e2fe8835101f65f89f7bbf464c8f
SahiMohite <126458089+SahiMohite@users.noreply.github.com>
2023-06-05 04:00:24 +0530

    commit changes

---
+++ out.js
@@ -0,0 +1,6 @@
+console.log("check");
+function fun(){
+    console.log("check");
+    document.getElementById("table1").style.visibility = "visible";
+    // document.getElementById("p").style.visibility = "hidden";
+} 
